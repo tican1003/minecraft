94a7e3ad58f29cd32c00d3c789910748447c7865
tican1003 <tricuong.can@gmail.com>
2022-01-04 13:14:04 +0700

    Add mobile menu, fix UI bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,37 @@ $(document).ready(function () {
     }
   });
 });
+
+// Mobile menu
+
+if (window.innerHeight < "740") {
+  var mobileMenuList = document.querySelector(".navbar__controls");
+  var mobileMenuBtn = document.querySelector(".navbar__menu");
+  var mobileMenuIcon = document.querySelector(".navbar__menu div");
+
+  mobileMenuBtn.onclick = function () {
+    if (mobileMenuList.style.display === "none") {
+      mobileMenuList.style.display = "block";
+      mobileMenuBtn.style.backgroundColor = "red";
+      mobileMenuIcon.classList.remove("ti-menu");
+      mobileMenuIcon.classList.add("ti-close");
+    } else {
+      mobileMenuList.style.display = "none";
+      mobileMenuBtn.style.backgroundColor = "#333";
+      mobileMenuIcon.classList.remove("ti-close");
+      mobileMenuIcon.classList.add("ti-menu");
+    }
+  };
+}
+
+// Mobile menu items
+
+var menuItems = document.querySelectorAll(".navbar__control-item a");
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].onclick = function () {
+    mobileMenuList.style.display = "none";
+    mobileMenuBtn.style.backgroundColor = "#333";
+    mobileMenuIcon.classList.remove("ti-close");
+    mobileMenuIcon.classList.add("ti-menu");
+  };
+}
